cb5dd4cb0634fc3443b8752704a40595b43b125e
ceoprogramer <esanchezsolano@byupathway.edu>
2025-09-04 15:48:00 -0600

    update Q6 y Q7

--- week01/scripts/activity01 copy.js
+++ week01/scripts/activity01 copy.js
@@ -6,6 +6,7 @@ const message = document.querySelector('aside');
 // Varibles for activity use
 let citynames = ["New York","Sacramento","Cleveland","South Bend","Tampa Bay","Corpus Christi"];
 let volume = 409;
+let quantity = document.querySelector('#q').value;
 
 function getCelsius(fahrenheit) {
 	return (fahrenheit - 32) * (5/9);
@@ -13,30 +14,37 @@ function getCelsius(fahrenheit) {
 
 // Set up the Date format object parameter for toLocaleDateString method.
 const options = {
-	  month: "short",
+		/*weekday: "long",
 		day: "numeric",
+		month: "long",
+		year: "numeric"*/
+		
+		day: "2-digit",
+		month: "short",
 		year: "numeric"
+
+
 	};
 
-// Question #1 
+// Question #1
 today1.innerHTML = new Date().toLocaleDateString("en-US", options);
 
-// Question #2 
+// Question #2
+//today2.innerHTML = "<strong>Volume</strong>: " + volume + " liters";
 today2.innerHTML = `<strong>Volume</strong>: ${volume} liters`;
 
 // Question #3
-let quantity = document.querySelector('#q').value;
+
 
 // Question #4
-document.querySelector('aside').innerHTML = 'Welcome to <mark>our</mark> neighborhood!';
+document.querySelector('aside').innerHTML = 'Welcome to <em>our</em> neighborhood!';
 
 // Question #5
-document.querySelector('#temp').value = `${getCelsius(33).toFixed(1)} °C`;
+document.querySelector('#temp').value = getCelsius(33);
 
 // Question #6
 const divs = document.querySelectorAll('div');
-document.querySelector('#divs').textContent = `${divs.length} divs in document.`;
-
 // Question #7
+
 let filterC = citynames.filter(city => city.charAt(0) === 'C');
-document.querySelector('#c-names').textContent = filterC;
+
