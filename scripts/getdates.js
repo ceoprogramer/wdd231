f5da53a9e8f6686bbb38546c26681eadc6beef8c
ceoprogramer <esanchezsolano@byupathway.edu>
2025-09-07 22:09:17 -0600

    Update certificate site

---
+++ scripts/getdates.js
@@ -0,0 +1,22 @@
+/**
+ * getdates.js
+ *
+ * This JavaScript update cuurent year and last modification date and display in footer
+ *
+ */
+// select the DOM elements for output
+
+const medium = document.querySelector("#medium");
+const year = document.querySelector("#year");
+
+// use the date object
+const today = new Date();
+
+medium.innerHTML = `Last Modification:  ${new Intl.DateTimeFormat(
+	"en-US",
+	{
+		dateStyle: "medium"
+	}
+).format(today)}`;
+
+year.innerHTML = ` ${today.getFullYear()}&copy; Erika Fabiola Sánchez Solano  Mérida Yuc. México.`;
