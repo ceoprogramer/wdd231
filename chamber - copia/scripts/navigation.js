75627eb42fec20604eaffbe42898f60bd10ddf7e
ceoprogramer <esanchezsolano@byupathway.edu>
2025-10-03 17:16:29 -0600

    update discover

--- chamber - copia/scripts/navigation.js
+++
@@ -1,8 +0,0 @@
-const navbutton = document.querySelector('#ham-btn');
-const navBar = document.querySelector('#nav-bar');
-
-navbutton.addEventListener('click',() =>{
-    navbutton.classList.toggle('show');
-    navBar.classList.toggle('show');  
-});
-
